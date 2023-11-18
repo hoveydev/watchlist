e6b1d5563147294a3d2954adc2b6e59370ffa5d2
Robert Hovey <robert.hovey95@gmail.com>
2023-11-18 13:37:26 -0500

    created simple splash screen

--- WatchList/WatchListApp.swift
+++ WatchList/WatchListApp.swift
@@ -4,7 +4,24 @@ import SwiftUI
 struct WatchListApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            AppStart()
         }
+    }
+}
+
+// implements the splash screen when the app is first opened
+struct AppStart: View {
+    @State private var isActive: Bool = false
+    
+    var body: some View {
+        !isActive ?
+        AnyView(
+            WatchListSplashScreen()
+                .onAppear {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                        self.isActive = true
+                    }
+                }
+        ) : AnyView(ContentView())
     }
 }
