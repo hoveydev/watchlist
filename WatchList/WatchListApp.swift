4a38a5764b9e41154ac42a8061ab7f062ffa1c59
Robert Hovey <robert.hovey95@gmail.com>
2023-11-18 10:28:44 -0500

    initial commit

---
+++ WatchList/WatchListApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchListApp.swift
+//  WatchList
+//
+//  Created by Robert Alec Hovey on 11/18/23.
+//
+
+import SwiftUI
+
+@main
+struct WatchListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
