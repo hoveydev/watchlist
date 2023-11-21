203a6e5cf5264981886f3660090299615b4ff819
Robert Hovey <robert.hovey95@gmail.com>
2023-11-21 08:54:34 -0500

    updated demo view for components

---
+++ Components/Sources/Components/WListNavBar.swift
@@ -0,0 +1,36 @@
+import SwiftUI
+
+struct WListNavBar: ViewModifier {
+    var text: String
+    var placement: ToolbarItemPlacement
+    var isMain: Bool
+    
+    func body(content: Content) -> some View {
+        content
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: placement) {
+                    HStack {
+                        isMain ? navText(text: text, font: .wListSubtitle) : navText(text: text, font: .wListNavTitle)
+                    }.padding()
+                }
+            }
+            .toolbarRole(.editor) // removes back text
+    }
+}
+
+struct navText: View {
+    var text: String
+    var font: Font
+
+    var body: some View {
+        WListText(text)
+            .font(font)
+    }
+}
+
+extension View {
+    func wListNavBar(_ text: String, placement: ToolbarItemPlacement, isMain: Bool) -> some View {
+        modifier(WListNavBar(text: text, placement: placement, isMain: isMain))
+    }
+}
