8d1fe545b0e7bcb1aa63fe7d3dbba6cbd3ccfe35
Robert Hovey <robert.hovey95@gmail.com>
2023-11-21 14:48:50 -0500

    edited package.swift for build

--- Package.swift
+++ Package.swift
@@ -14,9 +14,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .executableTarget(
             name: "WatchList",
-            dependencies: []),
+            dependencies: [],
+            path: "WatchList/Sources"),
         .testTarget(
             name: "WatchListTests",
-            dependencies: ["WatchList"]),
+            dependencies: ["WatchList"],
+            path: "WatchList/UnitTests"),
     ]
 )
