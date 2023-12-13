f61d02a1b08f268f51a487bf0b57db4aab504830
Robert Hovey <robert.hovey95@gmail.com>
2023-12-12 19:20:36 -0500

    set up new module

---
+++ WatchList/Modules/Login/LoginDemo/LoginDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginDemoApp.swift
+//  LoginDemo
+//
+//  Created by Robert Alec Hovey on 12/12/23.
+//
+
+import SwiftUI
+
+@main
+struct LoginDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
